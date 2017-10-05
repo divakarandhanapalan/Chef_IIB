131ccb6f9d652b1a9f1ee5688e3d15fbf01d8261
divakarandhanapalan <divakaran.dhanapalan@perficient.com>
2017-10-05 13:56:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Chef_IIB
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,64 @@
+################################################################################
+#
+# Copyright (c) 2013 IBM Corporation and other Contributors
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#     IBM - initial implementation
+#
+################################################################################
+#
+# Recipe IBM_Integratiom_Bus::default
+#
+# Installs all components required by IBM Integration Bus.
+#
+################################################################################
+
+log "Installing full development environment for IBM Integration Bus" do
+  level :info
+end
+if platform?("ubuntu")
+  execute "apt-get-update" do
+    command "apt-get update"
+    ignore_failure true
+  end
+else
+  execute "Update yum packages" do
+    user "root"
+    returns [0]
+    command "yum update -y"
+  end
+end
+
+log "Installing IBM Integration Bus Runtime" do
+  level :info
+end
+
+# Remove anything already installed
+iib_remove_components;
+iib_remove_runtime; 
+
+
+# Set up install environment
+iib_create_user;
+iib_setup_install_package;
+
+# Do the installs
+
+iib_install_runtime;
+
+
+log "Finished installing IBM Integration Bus Runtime" do
+  level :info
+end
+
+
+iib_remove_install_package;
+
+log "Finshed installing full development environment for IBM Integration Bus" do
+  level :info
+end
